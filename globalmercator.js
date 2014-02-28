aa91b12e46176552e036d135ef19521158876412
David <david.ershag@gmail.com>
2014-02-28 01:47:32 +0100

    Added files

---
+++ globalmercator.js
@@ -0,0 +1,95 @@
+/*global define, window */
+
+(function () {
+
+    "use strict";
+
+    var tileSize = 256,
+        initialResolution = 2 * Math.PI * 6378137 / tileSize,
+        originShift = 2 * Math.PI * 6378137 / 2,
+
+        mercator = {
+
+            tileSize: tileSize,
+
+            // Resolution (meters/pixel) for given zoom level (measured at Equator)
+            resolution: function (zoom) {
+                return initialResolution / Math.pow(2, zoom);
+            },
+
+            // Zoom level for given resolution (measured at Equator)
+            zoom: function (resolution) {
+                return Math.round(Math.log(initialResolution / resolution) / Math.log(2));
+            },
+
+            // Converts given lat/lon in WGS84 Datum to XY in Spherical Mercator EPSG:900913
+            latLonToMeters: function (lat, lon) {
+                var mx = lon * originShift / 180,
+                    my = Math.log(Math.tan((90 + lat) * Math.PI / 360)) / (Math.PI / 180);
+                my = my * originShift / 180;
+                return [mx, my];
+            },
+
+            // Converts XY point from Spherical Mercator EPSG:900913 to lat/lon in WGS84 Datum
+            metersToLatLon: function (mx, my) {
+                var lon = (mx / originShift) * 180,
+                    lat = (my / originShift) * 180;
+                lat = 180 / Math.PI * (2 * Math.atan(Math.exp(lat * Math.PI / 180)) - Math.PI / 2);
+                return [lat, lon];
+            },
+
+            // Converts pixel coordinates in given zoom level of pyramid to EPSG:900913
+            pixelsToMeters: function (px, py, zoom) {
+                var res = mercator.resolution(zoom),
+                    x = px * res - originShift,
+                    y = py * res - originShift; 
+                return [x, y];
+            },
+
+            // Converts EPSG:900913 to pyramid pixel coordinates in given zoom level
+            metersToPixels: function (mx, my, zoom) {
+                var res = mercator.resolution(zoom),
+                    x = (mx + originShift) / res,
+                    y = (my + originShift) / res;
+                return [x, y];
+            },
+
+            // Returns a tile covering region in given pixel coordinates
+            pixelsToTile: function (px, py) {
+                return [Math.floor(px / tileSize), Math.floor(py / tileSize)];
+            },
+
+            // Returns tile for given mercator coordinates
+            metersToTile: function (mx, my, zoom) {
+                var pixels = mercator.metersToPixels(mx, my, zoom);
+                return mercator.pixelsToTile(pixels[0], pixels[1]);
+            },
+
+            // Returns bounds of the given tile in EPSG:900913 coordinates
+            tileBounds: function (tx, ty, zoom) {
+                var min = mercator.pixelsToMeters(tx * tileSize, ty * tileSize, zoom),
+                    max = mercator.pixelsToMeters((tx + 1) * tileSize, (ty + 1) * tileSize, zoom);
+                return min.concat(max);
+            },
+
+            // Returns bounds of the given tile in latutude/longitude using WGS84 datum
+            tileLatLonBounds: function (tx, ty, zoom) {
+                var bounds = mercator.tileBounds(tx, ty, zoom),
+                    min = mercator.metersToLatLon(bounds[0], bounds[1]),
+                    max = mercator.metersToLatLon(bounds[2], bounds[3]);
+                return min.concat(max);
+            }
+        };
+
+    // export as AMD module / Node module / browser variable
+    if (typeof define === 'function' && define.amd) {
+        define(function () {
+            return mercator;
+        });
+    } else if (module !== undefined) {
+        module.exports = mercator;
+    } else {
+        window.mercator = mercator;
+    }
+
+}());
---
+++ test/globalmercator.test.js
@@ -0,0 +1,69 @@
+var assert = require('assert'),
+    mercator = require('../globalmercator');
+
+function aproxEqual(expected, actual) {
+    assert(Math.abs(expected - actual) < 0.0000000001, expected + ' != ' + actual);
+}
+
+function aproxArrayEqual(expected, actual) {
+    assert(expected.length === actual.length);
+    for (var i = 0; i < expected.length; ++i) {
+        aproxEqual(expected[i], actual[i]);
+    }
+}
+
+it('should return correct resolution', function () {
+    aproxEqual(156543.03392804097, mercator.resolution(0));
+    aproxEqual(78271.51696402048, mercator.resolution(1));
+    aproxEqual(39135.75848201024, mercator.resolution(2));
+    aproxEqual(19567.87924100512, mercator.resolution(3));
+    aproxEqual(9783.93962050256, mercator.resolution(4));
+    aproxEqual(4891.96981025128, mercator.resolution(5));
+});
+
+it('should return correct zoom level', function () {
+    aproxEqual(0, mercator.zoom(156543.03392804097));
+    aproxEqual(1, mercator.zoom(78271.51696402048));
+    aproxEqual(2, mercator.zoom(39135.75848201024));
+    aproxEqual(3, mercator.zoom(19567.87924100512));
+    aproxEqual(4, mercator.zoom(9783.93962050256));
+    aproxEqual(5, mercator.zoom(4891.96981025128));
+});
+
+it('should convert latlon to meters', function () {
+    aproxArrayEqual([1569604.8201851572, 8930630.669201756], mercator.latLonToMeters(62.3, 14.1));
+});
+
+it('should convert meters to latlon', function () {
+    aproxArrayEqual([62.3, 14.1], mercator.metersToLatLon(1569604.8201851572, 8930630.669201756));
+});
+
+it('should convert pixels to meters', function () {
+    aproxArrayEqual([0, 0], mercator.pixelsToMeters(128, 128, 0));
+    aproxArrayEqual([569754371.206588, 569754371.206588], mercator.pixelsToMeters(123456789, 123456789, 15));
+});
+
+it('should convert meters to pixels', function () {
+    aproxArrayEqual([128, 128], mercator.metersToPixels(0, 0, 0));
+    aproxArrayEqual([123456789, 123456789], mercator.metersToPixels(569754371.206588, 569754371.206588, 15));    
+});
+
+it('should convert pixels to tile', function () {
+    aproxArrayEqual([0, 0], mercator.pixelsToTile(128, 128));
+    aproxArrayEqual([482253, 482253], mercator.pixelsToTile(123456789, 123456789));
+});
+
+it('should convert meters to tile', function () {
+    aproxArrayEqual([0, 0], mercator.metersToTile(0, 0, 0));
+    aproxArrayEqual([482253, 482253], mercator.metersToTile(569754371.206588, 569754371.206588, 15));
+});
+
+it('should return tile bounds', function () {
+    var expected = [ 569754270.8829883, 569754270.8829883, 569755493.875441, 569755493.875441];
+    aproxArrayEqual(expected, mercator.tileBounds(482253, 482253, 15));
+});
+
+it('should return tile latlon bounds', function () {
+    aproxArrayEqual([ -85.05112877980659, -180, -85.05018093458115, -179.989013671875], mercator.tileLatLonBounds(0, 0, 15));
+    aproxArrayEqual([85.0511287798066, 180, 85.05207644397983, 180.010986328125], mercator.tileLatLonBounds(32768, 32768, 15));
+});
